240dbb2f733df8dd6e9b750897020cf9d6362ff6
Humberto MartinezBarron <31502370+HumbertoMartinezBarron@users.noreply.github.com>
2019-02-21 18:05:02 -0600

    Add files via upload

--- c/promedio/promedio.ino
+++ c/promedio/promedio.ino
@@ -1,23 +1,21 @@
-#include <stdio.h>
-
-main()
-{
-  int n, i, actual;
-  int sum=0;
-
-  clrscr();
-  printf("Promedio.n Cuantos numero quieres promediar: ");
-  scanf("%d",&n);
-
-  for(i=1;i<=n;i++)
-  {
-    printf("Numero:")
-    scanf("%d",&actual);
-    sum=sum+actual;
-  }
-
-  sum=sum/n;
-  printf("Promedio:"+sum);
-
-  return 0;
+void setup(){
+    int i, sum, actual;
+    int arreglo[]={1, 2, 3, 4};
+    double n=4;
+    sum=0;
+    i=0;
+    double prom=0;
+    
+    for(i=1;i<=n;i++) {
+      actual=arreglo[i];
+      sum=sum+actual;
+    }
+    prom=sum/n;
+    Serial.print(prom, DEC);
+  
 }
+void loop(){
+  
+}
+
+  
